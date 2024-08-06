973cf202c77adc6821e830cfd8140a51deafbf6c
Lucas <lucasbecca3@hotmail.com>
2024-08-06 19:25:37 -0300

    Arreglos

--- js/calculadora.js
+++ js/calculadora.js
@@ -2,38 +2,38 @@ const pantalla = document.getElementById('pantalla');
 const botones = document.querySelectorAll('.botones button');
 let operaciones = [];
 
-let operacion = '';
+let operacionActual = '';
 let numero1 = '';
 let numero2 = '';
-
+a
 botones.forEach(boton => {
-    boton.addEventListener('click', () => {
+    boton.onclick = function() {
         const valor = boton.textContent;
         switch (valor) {
             case '+':
             case '-':
             case '*':
             case '/':
-                operacion = valor;
+                operacionActual = valor;
                 numero1 = pantalla.value;
                 pantalla.value = '';
                 break;
             case '=':
                 numero2 = pantalla.value;
-                const resultado = eval(`${numero1} ${operacion} ${numero2}`);
+                const resultado = eval(`${numero1} ${operacionActual} ${numero2}`);
                 pantalla.value = resultado;
-                agregarOperacion(`${numero1} ${operacion} ${numero2} = ${resultado}`);
+                agregarOperacion(`${numero1} ${operacionActual} ${numero2} = ${resultado}`);
                 break;
             case 'C':
                 pantalla.value = '';
-                operacion = '';
+                operacionActual = '';
                 numero1 = '';
                 numero2 = '';
                 break;
             default:
                 pantalla.value += valor;
         }
-    });
+    };
 });
 
 function agregarOperacion(operacion) {
@@ -41,18 +41,18 @@ function agregarOperacion(operacion) {
     if (!historial) {
         const ul = document.createElement('ul');
         ul.id = 'historial';
-        ul.innerHTML = `<p>Ultimas Operaciones:</p><li>${operacion}</li>`;
+        ul.innerHTML = `<p>Últimas Operaciones:</p>`;
         document.body.appendChild(ul);
-    } else {
-        const li = document.createElement('li');
-        li.textContent = operacion;
-        historial.appendChild(li);
     }
+    const li = document.createElement('li');
+    li.textContent = operacion;
+    li.onclick = function() {
+        seleccionarOperacion(this);
+    };
+    historial.appendChild(li);
 }
 
-document.addEventListener('click', (e) => {
-    if (e.target.tagName === 'LI') {
-        const operacionSeleccionada = e.target.textContent;
-        pantalla.value = operacionSeleccionada.split('=')[0].trim();
-    }
-});
+function seleccionarOperacion(elemento) {
+    const operacionSeleccionada = elemento.textContent;
+    pantalla.value = operacionSeleccionada.split('=')[0].trim();
+}
