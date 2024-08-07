bb4ae124851bd93eff2ebfc11d04c140a090d7c9
Lucas <lucasbecca3@hotmail.com>
2024-08-07 20:24:46 -0300

    finalizado

--- js/calculadora.js
+++ js/calculadora.js
@@ -6,6 +6,8 @@ const pantallaInput = document.getElementById('pantalla');
 const botones = document.querySelectorAll('.botones button');
 const historial = document.getElementById('historial');
 
+
+
 botones.forEach(boton => {
     boton.onclick = function() {
         const valor = boton.textContent;
@@ -20,7 +22,7 @@ botones.forEach(boton => {
                 break;
             case '=':
                 numero2 = pantallaInput.value;
-                const resultado = eval(`${numero1} ${operacionActual} ${numero2}`);
+                const resultado = realizarOperacion(numero1, operacionActual, numero2);
                 pantallaInput.value = resultado;
                 agregarOperacion(`${numero1} ${operacionActual} ${numero2} = ${resultado}`);
                 break;
@@ -35,6 +37,31 @@ botones.forEach(boton => {
         }
     };
 });
+
+function realizarOperacion(num1, operacion, num2) {
+    let resultado;
+    switch (operacion) {
+        case '+':
+            resultado = parseFloat(num1) + parseFloat(num2);
+            break;
+        case '-':
+            resultado = parseFloat(num1) - parseFloat(num2);
+            break;
+        case '*':
+            resultado = parseFloat(num1) * parseFloat(num2);
+            break;
+        case '/':
+            if (num2 !== '0') {
+                resultado = parseFloat(num1) / parseFloat(num2);
+            } else {
+                resultado = 'Error: División por cero';
+            }
+            break;
+        default:
+            resultado = 'Error: Operación no válida';
+    }
+    return resultado;
+}
 
 function agregarOperacion(operacion) {
     const li = document.createElement('li');
